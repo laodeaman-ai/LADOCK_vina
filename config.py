4461cf0457035c309414469bf92d21b74d49da50
La Ode Aman <124052910+laodeaman-ai@users.noreply.github.com>
2023-07-20 00:12:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ ligand_dir = "Ligand" # nama direktori ligand
 target_dir = "Target" # nama direktori target
 mgltools_dir = "/home/arga/MGLTools-1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24"
 output_dir = "output"  # Nama direktori output yang ingin Anda buat
-ligand_center = False  # Ganti menjadi False jika tidak ingin menghasilkan file .pdbqt pusat berat ligand
-target_center = True  # Ganti menjadi True jika ingin menghasilkan file .pdbqt pusat berat target
 
 # Parameter simulasi docking vina
 size_x = 30.0
